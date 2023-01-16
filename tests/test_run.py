5393b4805c6604aab1093d7f00234a129d966100
Baptiste Pasquier <pasquierbaptiste@hotmail.fr>
2023-01-17 00:07:11 +0100

    :arrow_up::white_check_mark: upgrade poetry + add test

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,10 +1,11 @@
+import pickle
 from pathlib import Path
 
 from hickathon_2023.model import get_model
 from hickathon_2023.utils import load_data
 
 
-def test_fit_predict_small():
+def test_fit_predict():
     Y_FEATURE = "energy_consumption_per_annum"
     X_train, y_train = load_data(
         X_path=Path("datasets/small/train/train_features_sent.csv"),
@@ -16,5 +17,15 @@ def test_fit_predict_small():
     model = get_model()
 
     model.fit(X_train, y_train)
+
+    model.predict(X_test)
+
+
+def test_predict_from_trained():
+    X_test, _ = load_data(X_path=Path("datasets/small/test/test_features_sent.csv"))
+
+    trained_model_path = Path("trained_model/trained_model.pickle")
+    with open(trained_model_path, "rb") as f:
+        model = pickle.load(f)
 
     model.predict(X_test)
